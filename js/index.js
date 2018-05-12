1f18618712292e7dab8424d4a7bdfdb72d58d205
WeedDominator <bero4net@gmail.com>
2018-05-12 18:17:27 +0200

    finish

--- js/index.js
+++ js/index.js
@@ -26,15 +26,34 @@ class Cat {
     this.counter = 0;
   }
 
+  update(name, url, counter) {
+    this.name = name;
+    this.img = url;
+    this.counter = parseInt(counter, 10);
+  }
+
   render(id) {
     return (`
     <div class="col-9">
-      <div class="card" id="cat_pict_${id}" onclick="catClick(${id})">
+      <div class="card" id="cat_pict_${id}">
         <div class="card-header">
-          <h5 class="card-title">Name: ${this.name}</h5>
-          <h5 class="card-title">Clicked ${this.counter} Times</h5>
+          <div class="row">
+            <div class="col-9">
+              <h5 class="card-title">Name: ${this.name}</h5>
+              <h5 class="card-title">Clicked ${this.counter} Times</h5>
+            </div>
+            <div class="col-3 text-right">
+              <button 
+                type="button" 
+                class="btn btn-secondary"
+                onclick="adminPanel(1)"
+              >
+                Admin Panel
+              </button>
+            </div>
+          </div>
         </div>
-        <img class="card-img-top" src="${this.img}" alt="Cat">
+        <img class="card-img-top" src="${this.img}" alt="Cat" onclick="catClick()">
       </div>
     </div>`);
   }
@@ -65,21 +84,79 @@ const catList = () => {
   </div>`);
 };
 
+const adminPanelModal = () => {
+  const { name, img, counter } = Cats[selected];
+  return (`
+  <div class="modal" style="display:block;">
+    <div class="modal-dialog modal-sm">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Admin Panel</h5>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-3">
+              <p>Name:</p>
+            </div>
+            <div class="col-9 text-right">
+              <input id="text" type="text" class="form-control" value="${name}">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-3">
+              <p>Image:</p>
+            </div>
+            <div class="col-9 text-right">
+              <input id="url" type="text" class="form-control" value="${img}">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-3">
+              <p>Counter:</p>
+            </div>
+            <div class="col-9 text-right">
+              <input id="counter" type="number" min="0" class="form-control" value="${counter}">
+            </div>
+          </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" onclick="saveAdminAction()">Save changes</button>
+          <button type="button" class="btn btn-secondary" onclick="adminPanel(0)">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>`);
+};
+
 const Render = () => {
   let render = catList();
   render = render + Cats[selected].render(selected);
+  !openAdmin ? '' : render = render + adminPanelModal();
   container.innerHTML = render;
 };
 
 
-const catClick = (id) => {
-  Cats[id].counter += 1;
+const catClick = () => {
+  Cats[selected].counter += 1;
   Render();
 };
 
 const listClick = (id) => {
   selected = id;
   Render();
+};
+
+const adminPanel = (status) => {
+  openAdmin = !!status;
+  Render();
+};
+
+const saveAdminAction = () => {
+  Cats[selected].update(
+    document.querySelector('#text').value,
+    document.querySelector('#url').value,
+    document.querySelector('#counter').value,
+  );
+  adminPanel(0);
 };
 
 const Cats = catDB.map(cat => new Cat(cat.img, cat.name));
